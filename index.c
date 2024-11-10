f04047ce89dc8fff0234c37a89f0712f121ab730
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-10 23:38:08 +0200

    letters in string input

--- index.c
+++ index.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdio.h>
+
+#define MIN_LETTERS_IN_STRING 0
+#define MAX_LETTERS_IN_STRING 100
 
 int main() {
-    printf("Hello");
+    short int lettersInString = 0;
+
+    printf("Type the number of letters in one string:\n");
+
+    do {
+        if (scanf("%hd", &lettersInString) != 1) {
+            printf("Invalid input. Please enter an integer value.\n");
+            fflush(stdin);
+            continue;
+        }
+        fflush(stdin);
+
+        if (lettersInString <= MIN_LETTERS_IN_STRING || lettersInString > MAX_LETTERS_IN_STRING) {
+            printf("Invalid input for the number of letters in string. Please enter value greater and not equal to %d and less or equal to %d.\n",
+                   MIN_LETTERS_IN_STRING, MAX_LETTERS_IN_STRING);
+        }
+    } while (lettersInString <= MIN_LETTERS_IN_STRING || lettersInString > MAX_LETTERS_IN_STRING);
 
     return 0;
 }
 
-void sortStringsArray(char **stringsArray, int stringsNumber) {
-    int i, j;
 
-    for(i = 0; i < stringsNumber - 1; i++) {
-        for(j = i + 1; j < stringsNumber; j++) {
+void sortStringsArray(char **stringsArray, int stringsNumber) {
+    for(int i = 0; i < stringsNumber - 1; i++) {
+        for(int j = i + 1; j < stringsNumber; j++) {
             if (strcmp(stringsArray[i], stringsArray[j]) > 0) {
-                char *tmp;
-                tmp = stringsArray[i];
+                char *tmp = stringsArray[i];
                 stringsArray[i] = stringsArray[j];
                 stringsArray[j] = tmp;
             }
