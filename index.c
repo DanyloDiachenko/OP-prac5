c8dcdd1631cf5646e4599a55a413abeb36615ca4
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-11 17:42:10 +0200

    first version

--- index.c
+++ index.c
@@ -1,72 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
 
 #define MIN_LETTERS_IN_STRING 0
 #define MAX_LETTERS_IN_STRING 100
 #define MIN_NUMBER_OF_STRINGS 0
 #define MAX_NUMBER_OF_STRINGS 100
 
-int main() {
-    short int lettersInString = 0;
-    short int numberOfStrings = 0;
-    char isManualInputValue = 0;
-    bool isManualInput;
-
-    printf("If You want to put Your custom strings type 'c' and if You want strings be auto-generated type 'a':\n");
-    do {
-        isManualInputValue = getchar();
-        fflush(stdin);
-
-        if(isManualInputValue != 'c' && isManualInputValue != 'a') {
-            printf("Invalid input. Please type the value 'c' or 'a'.\n");
-            continue;
-        }
-
-        if(isManualInputValue == 'c') {
-            isManualInput = true;
-        } else {
-            isManualInput = false;
-        }
-    } while (isManualInputValue != 'c' && isManualInputValue != 'a');
-
-    printf("Type the number of letters in one string:\n");
-    do {
-        if (scanf("%hd", &lettersInString) != 1) {
-            printf("Invalid input. Please enter an integer value.\n");
-            fflush(stdin);
-            continue;
-        }
-        fflush(stdin);
-
-        if (lettersInString <= MIN_LETTERS_IN_STRING || lettersInString > MAX_LETTERS_IN_STRING) {
-            printf("Invalid input for the number of letters in string. Please enter value greater and not equal to %d and less or equal to %d.\n",
-                   MIN_LETTERS_IN_STRING, MAX_LETTERS_IN_STRING);
-        }
-    } while (lettersInString <= MIN_LETTERS_IN_STRING || lettersInString > MAX_LETTERS_IN_STRING);
-
-    printf("Type the number of strings:\n");
-    do {
-        if (scanf("%hd", &numberOfStrings) != 1) {
-            printf("Invalid input. Please enter an integer value.\n");
-            fflush(stdin);
-            continue;
-        }
-        fflush(stdin);
-
-        if (numberOfStrings <= MIN_NUMBER_OF_STRINGS || numberOfStrings > MAX_NUMBER_OF_STRINGS) {
-            printf("Invalid input for the number of strings. Please enter value greater and not equal to %d and less or equal to %d.\n",
-                   MIN_NUMBER_OF_STRINGS, MAX_NUMBER_OF_STRINGS);
-        }
-    } while (numberOfStrings <= MIN_NUMBER_OF_STRINGS || numberOfStrings > MAX_NUMBER_OF_STRINGS);
-
-    return 0;
-}
-
-
 void sortStringsArray(char **stringsArray, int stringsNumber) {
-    for(int i = 0; i < stringsNumber - 1; i++) {
-        for(int j = i + 1; j < stringsNumber; j++) {
+    for (int i = 0; i < stringsNumber - 1; i++) {
+        for (int j = i + 1; j < stringsNumber; j++) {
             if (strcmp(stringsArray[i], stringsArray[j]) > 0) {
                 char *tmp = stringsArray[i];
                 stringsArray[i] = stringsArray[j];
@@ -74,4 +20,114 @@ void sortStringsArray(char **stringsArray, int stringsNumber) {
             }
         }
     }
+}
+
+int getRandomNumber(unsigned short int rangeMin, unsigned short int rangeMax) {
+    return rangeMin + rand() % (rangeMax - rangeMin + 1);
+}
+
+int main() {
+    unsigned short int symbolsInStringNumber = 0;
+    unsigned short int stringsNumber = 0;
+    char isManualInputValue = 0;
+    bool isManualInput;
+
+    setlocale(LC_ALL, "");
+
+    printf("If you want to input custom strings type 'c', for auto-generated type 'a':\n");
+    do {
+        isManualInputValue = getchar();
+        fflush(stdin);
+
+        if (isManualInputValue != 'c' && isManualInputValue != 'a') {
+            printf("Invalid input. Please type 'c' or 'a'.\n");
+            continue;
+        }
+
+        isManualInput = isManualInputValue == 'c';
+    } while (isManualInputValue != 'c' && isManualInputValue != 'a');
+
+    printf("Type the number of letters in one string:\n");
+    do {
+        if (scanf("%hd", &symbolsInStringNumber) != 1) {
+            printf("Invalid input. Please enter an integer value.\n");
+            fflush(stdin);
+            continue;
+        }
+        fflush(stdin);
+
+        if (symbolsInStringNumber <= MIN_LETTERS_IN_STRING || symbolsInStringNumber > MAX_LETTERS_IN_STRING) {
+            printf("Please enter a value > %d and <= %d.\n", MIN_LETTERS_IN_STRING, MAX_LETTERS_IN_STRING);
+        }
+    } while (symbolsInStringNumber <= MIN_LETTERS_IN_STRING || symbolsInStringNumber > MAX_LETTERS_IN_STRING);
+
+    printf("Type the number of strings:\n");
+    do {
+        if (scanf("%hd", &stringsNumber) != 1) {
+            printf("Invalid input. Please enter an integer value.\n");
+            fflush(stdin);
+            continue;
+        }
+        fflush(stdin);
+
+        if (stringsNumber <= MIN_NUMBER_OF_STRINGS || stringsNumber > MAX_NUMBER_OF_STRINGS) {
+            printf("Please enter a value > %d and <= %d.\n", MIN_NUMBER_OF_STRINGS, MAX_NUMBER_OF_STRINGS);
+        }
+    } while (stringsNumber <= MIN_NUMBER_OF_STRINGS || stringsNumber > MAX_NUMBER_OF_STRINGS);
+
+    char str[stringsNumber][symbolsInStringNumber + 1];
+
+    if (isManualInput) {
+        printf("Enter strings:\n");
+        for (int i = 0; i < stringsNumber; i++) {
+            printf("String %d: ", i + 1);
+
+            if (fgets(str[i], symbolsInStringNumber + 1, stdin) == NULL || str[i][0] == '\n') {
+                fflush(stdin);
+                printf("Error: Input cannot be empty. Please enter a non-empty string.\n");
+                i--;
+                continue;
+            }
+            fflush(stdin);
+
+            str[i][strcspn(str[i], "\n")] = '\0';
+        }
+    } else {
+        unsigned short int randomSymbol = 0;
+        unsigned currentTimeInSeconds = time(NULL);
+        srand(currentTimeInSeconds);
+
+        for(int i = 0; i < stringsNumber; i++) {
+            unsigned short int currentSymbolIndex = 0;
+
+            do {
+                randomSymbol = getRandomNumber(65, 122);
+
+                if ((randomSymbol > 64 && randomSymbol < 91) || (randomSymbol > 96 && randomSymbol < 123)) {
+                    str[i][currentSymbolIndex] = randomSymbol;
+                    currentSymbolIndex++;
+                }
+            } while (currentSymbolIndex < symbolsInStringNumber);
+            str[i][currentSymbolIndex] = '\0';
+        }
+    }
+
+    printf("\nYour array:\n");
+    for (int i = 0; i < stringsNumber; i++) {
+        printf("%d -> %s\n", i + 1, str[i]);
+    }
+
+    char *stringPtrs[stringsNumber];
+    for (int i = 0; i < stringsNumber; i++) {
+        stringPtrs[i] = str[i];
+    }
+
+    sortStringsArray(stringPtrs, stringsNumber);
+
+    printf("\nSorted strings:\n");
+    for (int i = 0; i < stringsNumber; i++) {
+        printf("%d -> %s\n", i + 1, stringPtrs[i]);
+    }
+
+    return 0;
 }
