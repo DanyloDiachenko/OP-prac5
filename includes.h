b18fdf748e6df5dbfd8a224ff7634b0d306c6dcb
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-11 22:53:39 +0200

    code review

---
+++ includes.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+#include "./constants.h"
+#include "./functions.c"
+
+#endif // INCLUDES_H
--- index.c
+++ index.c
@@ -1,166 +1,33 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <time.h>
-#include <locale.h>
+#include "./includes.h"
 
-#define MIN_SYMBOLS_IN_STRING 1
-#define MAX_SYMBOLS_IN_STRING 1000
-#define MIN_NUMBER_OF_STRINGS 1
-#define MAX_NUMBER_OF_STRINGS 10000
-#define SPACE_SYMBOL_CODE 32
-#define TILDA_SYMBOL_CODE 126
-
-void sortStringsArray(char **stringsArray, int stringsNumber) {
-    for (int i = 0; i < stringsNumber - 1; i++) {
-        for (int j = i + 1; j < stringsNumber; j++) {
-            if (strcmp(stringsArray[i], stringsArray[j]) > 0) {
-                char *tmp = stringsArray[i];
-
-                stringsArray[i] = stringsArray[j];
-                stringsArray[j] = tmp;
-            }
-        }
-    }
-}
-
-int getRandomNumber(int rangeMin, int rangeMax) {
-    return rangeMin + rand() % (rangeMax - rangeMin + 1);
-}
-
-int getRandomSymbol() {
-    return getRandomNumber(SPACE_SYMBOL_CODE, TILDA_SYMBOL_CODE);
-}
+bool getAndValidateIsManualInput();
+int getAndValidateSymbolsInString();
+int getAndValidateStringsNumber();
+void printResults(int stringsNumber, char **strings);
+void freeMemory(int stringsNumber, char **strings);
+void getStrings(bool isManualInput, int stringsNumber, char **strings, int symbolsInStringNumber);
+char **allocateStringArray(int stringsNumber);
+char **allocateMemoryForStrings(int stringsNumber, int symbolsInStringNumber);
 
 int main() {
-    int symbolsInStringNumber = 0;
-    int stringsNumber = 0;
-    char isManualInputValue = 0;
-    bool isManualInput = false;
+    printf("This program sorts strings\n");
 
     setlocale(LC_ALL, "");
 
-    printf("If you want to input custom strings type 'c', for auto-generated type 'a':\n");
-    do {
-        isManualInputValue = getchar();
-        fflush(stdin);
+    bool isManualInput = getAndValidateIsManualInput();
+    int symbolsInStringNumber = getAndValidateSymbolsInString();
+    int stringsNumber = getAndValidateStringsNumber();
 
-        if (isManualInputValue != 'c' && isManualInputValue != 'a') {
-            printf("Invalid input. Please type 'c' or 'a'.\n");
-
-            continue;
-        }
-
-        isManualInput = isManualInputValue == 'c';
-    } while (isManualInputValue != 'c' && isManualInputValue != 'a');
-
-    printf("Type the number of letters in one string:\n");
-    do {
-        if (scanf("%d", &symbolsInStringNumber) != 1) {
-            printf("Invalid input. Please enter an integer value.\n");
-            fflush(stdin);
-
-            continue;
-        }
-        fflush(stdin);
-
-        if (symbolsInStringNumber <= MIN_SYMBOLS_IN_STRING || symbolsInStringNumber > MAX_SYMBOLS_IN_STRING) {
-            printf("Please enter a value > %d and <= %d.\n", MIN_SYMBOLS_IN_STRING, MAX_SYMBOLS_IN_STRING);
-        }
-    } while (symbolsInStringNumber <= MIN_SYMBOLS_IN_STRING || symbolsInStringNumber > MAX_SYMBOLS_IN_STRING);
-
-    printf("Type the number of strings:\n");
-    do {
-        if (scanf("%d", &stringsNumber) != 1) {
-            printf("Invalid input. Please enter an integer value.\n");
-            fflush(stdin);
-
-            continue;
-        }
-        fflush(stdin);
-
-        if (stringsNumber <= MIN_NUMBER_OF_STRINGS || stringsNumber > MAX_NUMBER_OF_STRINGS) {
-            printf("Please enter a value > %d and <= %d.\n", MIN_NUMBER_OF_STRINGS, MAX_NUMBER_OF_STRINGS);
-        }
-    } while (stringsNumber <= MIN_NUMBER_OF_STRINGS || stringsNumber > MAX_NUMBER_OF_STRINGS);
-
-    char **strings = malloc(stringsNumber * sizeof(char *));
+    char **strings = allocateMemoryForStrings(stringsNumber, symbolsInStringNumber);
     if (strings == NULL) {
-        printf("Memory allocation failed!\n");
-        printf("Type any key to close the program\n");
-        getchar();
-
         return 0;
     }
 
-    for (int i = 0; i < stringsNumber; i++) {
-        strings[i] = (char *)malloc((symbolsInStringNumber + 1) * sizeof(char));
+    getStrings(isManualInput, stringsNumber, strings, symbolsInStringNumber);
 
-        if (strings[i] == NULL) {
-            printf("Memory allocation failed!\n");
+    printResults(stringsNumber, strings);
 
-            for (int j = 0; j < i; j++) {
-                free(strings[j]);
-            }
-            free(strings);
-
-            printf("Type any key to close the program\n");
-            getchar();
-
-            return 0;
-        }
-    }
-
-    if (isManualInput) {
-        printf("Enter strings:\n");
-        for (int i = 0; i < stringsNumber; i++) {
-            printf("String %d: ", i + 1);
-
-            if (fgets(strings[i], symbolsInStringNumber + 1, stdin) == NULL || strings[i][0] == '\n') {
-                fflush(stdin);
-                printf("Error: Input cannot be empty. Please enter a non-empty string.\n");
-                i--;
-
-                continue;
-            }
-            fflush(stdin);
-
-            strings[i][strcspn(strings[i], "\n")] = '\0';
-        }
-    } else {
-        srand(time(NULL));
-
-        for (int i = 0; i < stringsNumber; i++) {
-            int currentSymbolIndex = 0;
-
-            do {
-                int randomSymbol = getRandomSymbol();
-
-                strings[i][currentSymbolIndex] = randomSymbol;
-                currentSymbolIndex++;
-            } while (currentSymbolIndex < symbolsInStringNumber);
-
-            strings[i][currentSymbolIndex] = '\0';
-        }
-    }
-
-    printf("\nYour array:\n");
-    for (int i = 0; i < stringsNumber; i++) {
-        printf("%d -> %s\n", i + 1, strings[i]);
-    }
-
-    sortStringsArray(strings, stringsNumber);
-
-    printf("\nSorted strings:\n");
-    for (int i = 0; i < stringsNumber; i++) {
-        printf("%d -> %s\n", i + 1, strings[i]);
-    }
-
-    for (int i = 0; i < stringsNumber; i++) {
-        free(strings[i]);
-    }
-    free(strings);
+    freeMemory(stringsNumber, strings);
 
     return 0;
 }
